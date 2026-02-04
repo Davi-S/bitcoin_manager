c8e99bd3f21388e3f66f1f9196569b248642cccf
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-02-04 00:39:41 -0300

    return to default behaviour

--- bitcoin_manager/main.py
+++ bitcoin_manager/main.py
@@ -1,27 +1,20 @@
 from . import private_key as pv
 from . import address
-import random
-import pathlib
+
 
 def main():
     # Open and read the private key file
-    # with open("private_key.txt", "r") as f:
-    #     private_key_bits = f.read().strip()
+    with open("private_key.txt", "r") as f:
+        private_key_bits = f.read().strip()
 
-    for i in range(20):
-        random_bytes = random.randbytes(32)
+    # Create a private key object from the binary string
+    private_key = pv.PrivateKey.from_bits(private_key_bits)
+    print(f"Private key bits: {private_key.to_bits}")
 
-        # Create a private key object from the binary string
-        private_key = pv.PrivateKey.from_bytes(random_bytes)
-        print(private_key.to_hex)
+    # Generate the taproot address
+    taproot_address = address.get_taproot_address(private_key.to_bytes)
+    print(f"Address: {taproot_address}")
 
-        # Generate the taproot address
-        taproot_address = address.get_taproot_address(private_key.to_bytes)
-        print(taproot_address)
-
-        with open(pathlib.Path(".").joinpath(f"wallet_{i+1}"), "w") as f:
-            content = f"{private_key.to_wif_compressed}\n{taproot_address}"
-            f.write(content)
 
 if __name__ == "__main__":
     main()
