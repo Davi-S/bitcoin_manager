7bc6155b747b5753b712a54a41ce0a58da9febaa
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-02-05 02:35:13 -0300

    Add Wallet

---
+++ bitcoin_manager/wallet.py
@@ -0,0 +1,45 @@
+import dataclasses
+
+from . import address
+from . import private_key
+from . import public_key
+
+
+@dataclasses.dataclass(frozen=True)
+class Wallet:
+    """Represents a Bitcoin wallet with a private key, public key, and Taproot address."""
+
+    _private_key: private_key.PrivateKey
+    _public_key: public_key.PublicKey = dataclasses.field(init=False, repr=False)
+    _address: str = dataclasses.field(init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        # Derive public key from private key
+        derived_public_key = public_key.PublicKey.from_private_key(self._private_key)
+        
+        # Generate Taproot address
+        taproot_address = address.get_taproot_address(self._private_key.to_bytes)
+        
+        # Set the derived fields using object.__setattr__ for frozen dataclass
+        object.__setattr__(self, "_public_key", derived_public_key)
+        object.__setattr__(self, "_address", taproot_address)
+
+    @classmethod
+    def from_private_key(cls, priv_key: private_key.PrivateKey) -> "Wallet":
+        """Create a wallet from a PrivateKey instance."""
+        return cls(priv_key)
+
+    @property
+    def private_key(self) -> private_key.PrivateKey:
+        """Return the private key."""
+        return self._private_key
+
+    @property
+    def public_key(self) -> public_key.PublicKey:
+        """Return the public key."""
+        return self._public_key
+
+    @property
+    def address(self) -> str:
+        """Return the Taproot address."""
+        return self._address
